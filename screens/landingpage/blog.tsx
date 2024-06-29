ad2eb51af9f4ce5d9571f4961abfe69184bec0b8
Abdullah Hamid <hafizabdullahqurashi1@gmail.com>
2024-06-29 13:27:14 +0500

    update

---
+++ screens/landingpage/blog.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+  <div className="blog-parent">
+    <div className="background">
+        <div className="width">
+        <div className="title">
+          <div>
+            <div>
+              <h1>frequently asked questions</h1>
+            </div>
+            <div className="img">
+              <img src="./images/divider.png" alt="" />
+            </div>
+          </div>
+        </div>
+        </div>
+    </div>
+  </div>
+  )
+}
+
+export default blog
