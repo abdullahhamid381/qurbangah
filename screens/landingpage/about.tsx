a3738998b6e194e10b2375c0012e771e614d319e
Abdullah Hamid <hafizabdullahqurashi1@gmail.com>
2024-06-29 21:29:13 +0500

    update

--- screens/landingpage/about.tsx
+++ screens/landingpage/about.tsx
@@ -22,11 +22,12 @@ const about = () => {
             </div>
             <div className="para">
               <h1>Our Vision</h1>
-              <p>
+             
+              <p>Qurbangah will put an affort to provide income and oppurtunity to women farmers focus in south punjab. At the start of this adventure it might seems like a drop in the ocean but our goal is to contribute in poverty alliviation.  
                 Our mission is to create a seamless and meaningful experience
                 for individuals looking to fulfill their religious duties during
                 Eid ul Adha. By offering a user-friendly platform, we make it
-                easy for you to sponsor animals for sacrifice, ensuring that
+                easy for you to purchase animals for sacrifice, ensuring that
                 your contributions are used ethically and transparently to
                 benefit those in need.
               </p>
