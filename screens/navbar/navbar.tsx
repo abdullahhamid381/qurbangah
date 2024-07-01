5a68968a0dfd1033cd7d6140db1398273e20b583
Abdullah Hamid <hafizabdullahqurashi1@gmail.com>
2024-07-01 16:37:37 +0500

    update

--- screens/navbar/navbar.tsx
+++ screens/navbar/navbar.tsx
@@ -66,17 +66,14 @@ function Navbar() {
                 <img src="./images/logo.png" alt="Logo" />
               </div>
               <div className="browse-flex">
-                {navbardata.map((item) => {
-                  return (
-                    <li className="nav-item">
+              
+                 <li className="nav-item">
                       <div className="nav-links browse" onClick={handleClick}>
-                        <Link href={item.link}>
+                        <Link href=''>
                           <p className="link">{item.item}</p>
                         </Link>
                       </div>
                     </li>
-                  );
-                })}
               </div>
             </ul>
           </div>
